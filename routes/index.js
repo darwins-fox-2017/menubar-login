f7fdaa5cf3687d7e8cb99deb14afdca3d34bf7f5
Eri Irawan <eryirawan91@gmail.com>
2017-02-23 10:43:43 +0700

    create view index & user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ei Corps' });
 });
+
+router.post('/new', function(req, res){
+  models.User.create(req.body).then(function(users){
+    res.render('user')
+  })
+})
 
 module.exports = router;
