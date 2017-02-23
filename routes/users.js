10ba59e326246a4f327817702f35ffcffad50d6c
Diky Arga <dikyarga.id@gmail.com>
2017-02-23 12:51:33 +0700

    add views homepage, login and register page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+let model = require('../models')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
   res.send('respond with a resource');
 });
+
+router.post('/create', function(res, req, next){
+  console.log(res.body);
+
+})
 
 module.exports = router;
